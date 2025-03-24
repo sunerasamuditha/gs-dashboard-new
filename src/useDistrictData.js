e30a94d5aa7f10df80df33863a92dba1b0ebde66
Sunera Samuditha Godakanda <sunerasandasiri@gmail.com>
2025-03-25 00:29:07 +0530

    Update useDistrictData.js

--- src/useDistrictData.js
+++ src/useDistrictData.js
@@ -122,7 +122,7 @@ const useDistrictData = () => {
   };
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/sheet-data')
+    fetch('https://gsdashboard-backend.onrender.com/api/sheet-data')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not OK');
